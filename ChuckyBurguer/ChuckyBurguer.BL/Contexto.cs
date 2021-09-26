78cd0875314b55d2f97db462e800225dc476d122
Henry La Chira <henrylachira1995@gmail.com>
2021-09-26 14:48:36 -0600

    Tarea 2 / agregando Entity F. Code First

---
+++ ChuckyBurguer/ChuckyBurguer.BL/Contexto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChuckyBurguer.BL
+{                    //Herencia
+    public class Contexto : DbContext
+    {
+        //constructor
+        public Contexto(): base(@"Data Source=(LocalDb)\MSSQLLocalDB;AttachDBFilename=" +
+            Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\ChuckyBurguerDB.mdf")
+        {
+            
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+
+        //propiedad
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
