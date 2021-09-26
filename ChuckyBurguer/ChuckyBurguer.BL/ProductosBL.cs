4683afcd0b325b172e8e0b32b2654bcf9ee7fde2
Henry La Chira <henrylachira1995@gmail.com>
2021-09-26 13:16:41 -0600

    Agregando arquitectura basica

---
+++ ChuckyBurguer/ChuckyBurguer.BL/ProductosBL.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChuckyBurguer.BL
+{
+    public class ProductosBL
+    {
+        //funcion ue devuelve lista de productos
+        public List<Producto> ObtenerProductos()
+        {
+            var producto1 = new Producto();
+            producto1.Id = 1;
+            producto1.Descripcion = "Hamburguesa Doble Carne";
+            producto1.Precio = 250;
+
+            var producto2 = new Producto();
+            producto2.Id = 2;
+            producto2.Descripcion = "Chucky Burguer Economico";
+            producto2.Precio = 125;
+
+            var producto3 = new Producto();
+            producto3.Id = 3;
+            producto3.Descripcion = "Burguer Roja";
+            producto3.Precio = 245; 
+
+            var listadeProductos = new List<Producto>();
+            listadeProductos.Add(producto1);
+            listadeProductos.Add(producto2);
+            listadeProductos.Add(producto3);
+
+            return listadeProductos;
+        }
+    }
+}
---
+++ ChuckyBurguer/ChuckyBurguer.WebAdmin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ChuckyBurguer.WebAdmin.MvcApplication" Language="C#" %>
