cfc42954cc2f3e0004c7f13c097f99e9c7c8fdf7
Henry La Chira <henrylachira1995@gmail.com>
2021-11-22 19:56:46 -0600

    Agregando Contraseñas

--- ChuckyBurguer/ChuckyBurguer.BL/DatosdeInicio.cs
+++ ChuckyBurguer/ChuckyBurguer.BL/DatosdeInicio.cs
@@ -15,10 +15,22 @@ namespace ChuckyBurguer.BL
             nuevoUsuario.Nombre = "admin";
             nuevoUsuario.Contrasena = Encriptar.CodificarContrasena("123");
 
-           
+            var nuevoUsuario1 = new Usuario();
+            nuevoUsuario1.Nombre = "Henry";
+            nuevoUsuario1.Contrasena = Encriptar.CodificarContrasena("195");
+
+            var nuevoUsuario2 = new Usuario();
+            nuevoUsuario2.Nombre = "Allan";
+            nuevoUsuario2.Contrasena = Encriptar.CodificarContrasena("181");
+
+            var nuevoUsuario3 = new Usuario();
+            nuevoUsuario3.Nombre = "Billy";
+            nuevoUsuario3.Contrasena = Encriptar.CodificarContrasena("201");
 
             contexto.Usuarios.Add(nuevoUsuario);
-            
+            contexto.Usuarios.Add(nuevoUsuario1);
+            contexto.Usuarios.Add(nuevoUsuario2);
+            contexto.Usuarios.Add(nuevoUsuario3);
 
             base.Seed(contexto);
         }
